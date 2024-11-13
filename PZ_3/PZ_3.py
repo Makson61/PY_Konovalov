6c172b773ac86ccb8e472c7fb2fb28f441ba5abd
Makson61 <maksimkonovalov0289@gmail.com>
2024-11-13 22:56:43 +0300

    Залил PZ_3

--- PZ_3/PZ_3.py
+++ PZ_3/PZ_3.py
@@ -1,6 +1,5 @@
 #Даны три целых числа: A, B, C. Проверить истинность высказывания
-def possitivest():
-     try:
+try:
         A = int(input("Введите число A: "))
         B = int(input("Введите число B: "))
         C = int(input("Введите число C: "))
@@ -11,10 +10,9 @@ def possitivest():
             possitive += 1
         if C > 0:
             possitive += 1
-        return possitive == 2
-     except:
+        if possitive == 2:
+            print("Ровно два из чисел A,B,C являются положительными")
+        else:
+            print("Высказывание неверно")
+except ValueError:
          print("Произошла ошибка, повторите попытку")
-if possitivest():
-    print("Ровно два из чисел A,B,C являются положительными")
-else:
-    print("Высказывание неверно")
