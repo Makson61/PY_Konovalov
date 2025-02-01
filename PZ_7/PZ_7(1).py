854da905fb14bda2750bc52bba5b6d1a7fef8fe4
Albus228 <danilakir1313123@mail.ru>
2025-02-01 18:17:07 +0300

    Перезалил PZ_7

--- PZ_7/PZ_7(1).py
+++ PZ_7/PZ_7(1).py
@@ -1,21 +1,14 @@
-# Функция для преобразования строки
+#Дана строка. Преобразовать в ней все прописные латинские буквы в строчные.
+
 def convert_case(input_string):
-    return input_string.lower()  # Приводим строку к нижнему регистру
+    return input_string.lower()
 
-# Основной блок программы
 try:
-    # Запрашиваем ввод от пользователя
     user_input = input("Введите строку: ")
-
-    # Проверяем, что ввод не пустой
     if user_input == "":
         print("Ошибка: Ввод не должен быть пустым.")
     else:
-        # Преобразуем строку
         converted_string = convert_case(user_input)
-
-        # Выводим результат
         print("Преобразованная строка:", converted_string)
-
 except ValueError:
     print("Ошибка: Неверный ввод.")
