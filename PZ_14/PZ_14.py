21db10d9c955e2795e2aef360f9fddc5096d9f1f
Makson61 <maksimkonovalov0289@gmail.com>
2025-06-13 22:44:22 +0300

    Перезалил PZ_14

--- PZ_14/PZ_14.py
+++ PZ_14/PZ_14.py
@@ -1,18 +1,15 @@
 # Из исходного текстового файла (price.txt) выбрать все цены. Посчитать количество
 # полученных элементов.
-def is_float(string):
-    try:
-        float(string)
-        return True
-    except ValueError:
-        return False
+import re
 
 
 def count_prices(filename):
     with open(filename, 'r', encoding='utf-8') as file:
-        words = file.read().split()
-        prices = list(filter(lambda x: x.replace('.', '', 1).isdigit(), words))
-        valid_prices = list(filter(is_float, prices))
+        text = file.read()
+        # Ищем все числа с десятичной точкой или без
+        prices = re.findall(r'\b\d+\.?\d*\b', text)
+        # Фильтруем, оставляя только валидные числа (исключаем случаи с точкой в конце)
+        valid_prices = [price for price in prices if '.' not in price or re.match(r'^\d+\.\d+$', price)]
 
     return len(valid_prices)
 
